c3e9f89bd3599fbfb5f5a6ae1565ae9e92b7f2d2
Dinesh Keserla <50540419+dkeserla@users.noreply.github.com>
2019-09-04 17:40:40 -0500

    Batch File
    Batch File to run Python Script to get all the assignments then opens the txt file containing the information about the scripts.

---
+++ Reminder.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+C:\Users\Dinesh\AppData\Local\Programs\Python\Python37-32\python.exe C:\Users\Dinesh\git\Assignment-Updater\__main__.py %*
+start notepad "C:\Users\Dinesh\git\Assignment-Updater\reminders.txt"
+exit
