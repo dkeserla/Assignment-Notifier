764eb8eeaab1fe1a6ee163854ed73a399695bd94
Dinesh Keserla <50540419+dkeserla@users.noreply.github.com>
2019-11-28 15:46:03 -0600

    Updated Classes see Weighting of Points

--- assignments.py
+++ assignments.py
@@ -6,14 +6,18 @@ def get_assignments(assignments):
     class_assignments = {}
     for assignment in class_assignments_containers:
         class_string = clean_string(assignment.find_all(attrs={'class': 'sg-header-heading'})[0].text)
+        class_grade = assignment.find_all(attrs={'class': 'sg-header-heading sg-right'})[0].text
+        class_grade = class_grade[class_grade.rfind(" ")+1:].strip()
+        course = Class(class_string, class_grade)
         assigns = assignment.find_all(attrs={'class': 'sg-asp-table-data-row'})
         assignment_data = []
         for assign in assigns:
             list = assign.find_all('a')
             for a in list:
                 nums = re.findall('[0-9.]+', str(assign.find_all('td')[4]))
-                assignment_data.append(Assignment(a.attrs['title'], nums))
-        class_assignments[class_string] = assignment_data
+                total = nums = re.findall('[0-9.]+', str(assign.find_all('td')[5]))
+                assignment_data.append(Assignment(a.attrs['title'], nums, total))
+        class_assignments[course] = assignment_data
     # for course in class_assignments:
     #     print(course)
     #     for assignment in class_assignments[course]:
@@ -28,15 +32,28 @@ def clean_string(class_string):
     class_string = class_string[class_string.rfind(last_num)+1:].strip()
     return class_string
 
+class Class:
+    name = ''
+    grade = ''
+
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
+
+    def __str__(self):
+        return self.name + " - " + self.grade
+
 class Assignment:
     classwork = ''
     category = ''
     due_date = ''
     grade = ''
+    total_points = ''
 
-    def __init__(self, title_vals, grade):
+    def __init__(self, title_vals, grade, total_points):
         self.classwork, self.category, self.due_date = self.parse_elements(title_vals)
         self.grade = 'not put in yet' if len(grade) == 0 else "{:.2f}".format(float(grade[0]))
+        self.total_points = "" if self.grade == 'not put in yet' else "{:.2f}".format(float(total_points[0]))
 
     def parse_elements(self, title_vals):
         assignment_details = []
@@ -48,4 +65,5 @@ class Assignment:
         return assignment_details[0], assignment_details[1], assignment_details[2]
 
     def __str__(self):
-        return 'classwork is ' + self.classwork + ', category is ' + self.category + ', due date is ' + self.due_date + ', and your grade is ' + self.grade
+        out = 'classwork is ' + self.classwork + ', category is ' + self.category + ', due date is ' + self.due_date + ', and your grade is ' + self.grade
+        return out + " / " + self.total_points if self.total_points != "" else out
